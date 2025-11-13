acd1d716597eb9e2f029f39a3a3dbde6608d0775
mourad <mouradzerbane@gmail.com>
2025-11-13 03:12:50 +0100

    Add TestMemoize class to test memoization decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ Unit tests for utils module
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -65,6 +65,44 @@ class TestGetJson(unittest.TestCase):
             
             # Test that the output equals test_payload
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test class for utils.memoize decorator
+    """
+
+    def test_memoize(self):
+        """
+        Test that memoize decorator caches the result
+        """
+        class TestClass:
+            """
+            Test class with memoized property
+            """
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # Create instance of TestClass
+        test_instance = TestClass()
+        
+        # Patch a_method to track calls and return 42
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            # First call to a_property
+            result1 = test_instance.a_property
+            # Second call to a_property
+            result2 = test_instance.a_property
+            
+            # Test that both results are 42
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            
+            # Test that a_method was called only once (due to memoization)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':

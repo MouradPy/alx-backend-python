a09f3b041cb3a6cc9f5fc2f85f66ad964a038f1d
mourad <mouradzerbane@gmail.com>
2025-11-16 00:12:04 +0100

    Add URL routes for chats API endpoints

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for messaging_app project.
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+# Create a router and register our viewsets
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'conversations', views.ConversationViewSet, basename='conversation')
+router.register(r'messages', views.MessageViewSet, basename='message')
 
+# The API URLs are now determined automatically by the router
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
